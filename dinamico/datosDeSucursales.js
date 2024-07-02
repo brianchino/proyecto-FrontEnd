091be11377ef67e928d213e0a1e7afba5880c855
chino brian <bchinoyanarico@frba.utn.edu.ar>
2024-07-02 19:41:02 -0300

    probando

--- dinamico/datosDeSucursales.js
+++ dinamico/datosDeSucursales.js
@@ -1,5 +1,5 @@
 // origen de los datos
-const origen = "http://chinobrian.pythonanywhere.com/api-proyecto/sucursales"
+const origen = "https://chinobrian.pythonanywhere.com/api-proyecto/sucursales"
 
 //evento a escuchar
 window.addEventListener('DOMContentLoaded', evento =>{
